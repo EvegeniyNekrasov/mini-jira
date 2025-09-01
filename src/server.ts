a123802458fe02b9bcb4c4758eea348a59fc0b34
EvegeniyNekrasov <eanekrasovart@gmail.com>
2025-09-01 17:53:14 +0200

    adding middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 import "dotenv/config";
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
 
 const app = express();
-const PORT = 6969;
+const PORT = Number(process.env.PORT) || 5000;
+
+// Middlewares
+app.use(helmet());
+app.use(compression());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({ message: "hi" });
 });
-app.listen(PORT, () => console.log("listening on http://localhost:6969"));
+app.listen(PORT, () =>
+    console.log(`MINI JIRA: listening on http://localhost:${PORT}`)
+);
